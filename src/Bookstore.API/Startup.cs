c84b5235e908849891da2076caa0552323ef105c
Mehdi <mohamedmehdi.khelil@insat.ucar.tn>
2022-08-09 22:27:09 +0100

    so

--- src/Bookstore.API/Startup.cs
+++ src/Bookstore.API/Startup.cs
@@ -1,25 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using BookStore.Infrastructure.Context;
+
 namespace Bookstore.API
 {
-    public void ConfigureServices(IServiceCollection services)
-    {
-        services.AddDbContext<BookStoreDbContext>(options =>
-        {
-            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
-        });
-
-        services.AddControllers();
-    }
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -28,11 +16,15 @@ namespace Bookstore.API
         }
 
         public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddDbContext<BookStoreDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
