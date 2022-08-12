7e46bf6715571674a8b42824b14bec15fb4de50c
Mehdi <mohamedmehdi.khelil@insat.ucar.tn>
2022-08-12 14:19:47 +0100

    4th part

--- src/Bookstore.API/Startup.cs
+++ src/Bookstore.API/Startup.cs
@@ -6,6 +6,9 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using BookStore.Infrastructure.Context;
 using Microsoft.EntityFrameworkCore.Design;
+using BookStore.API.Configuration;
+using AutoMapper;
+using Microsoft.OpenApi.Models;
 namespace Bookstore.API
 {
     public class Startup
@@ -17,6 +20,7 @@ namespace Bookstore.API
 
         public IConfiguration Configuration { get; }
         // This method gets called by the runtime. Use this method to add services to the container.
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<BookStoreDbContext>(options =>
@@ -24,7 +28,19 @@ namespace Bookstore.API
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
             });
 
+            services.AddAutoMapper(typeof(Startup));
+
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Title = "BookStore API",
+                    Version = "v1"
+                });
+            });
+            services.ResolveDependencies();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,9 +56,12 @@ namespace Bookstore.API
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
 
             app.UseRouting();
 
@@ -50,7 +69,7 @@ namespace Bookstore.API
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
         }
     }
